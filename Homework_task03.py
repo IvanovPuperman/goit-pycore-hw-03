fb143b9dd71bc34e0279471de7f8c7e23ab81fb4
Volodymyr Ivanov <64498269+IvanovPuperman@users.noreply.github.com>
2026-03-08 14:19:42 +0000

    Homework_task03

---
+++ Homework_task03.py
@@ -0,0 +1,9 @@
+import re
+
+def normalize_phone(phone_number):
+    phone = re.sub(r"[^\d]", "", phone_number)
+
+    if phone.startswith("380"):
+        return "+" + phone
+
+    return "+38" + phone
